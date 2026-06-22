aa6dbaddfda168e9b1f696f8cdcf667ea192b578
GyarmatiMark <gyarmati.mark22@gmail.com>
2026-06-22 19:19:36 +0200

    final boss hp
    a final bossnak van 5 hpja

--- Assets/scriptek/enemy_scripts/final_boss.cs
+++ Assets/scriptek/enemy_scripts/final_boss.cs
@@ -5,28 +5,31 @@ public class EnemyFinalBoss : MonoBehaviour
     public int damage = 1;
     public float bounceForce = 10f;
 
+    [Header("Boss Stats")]
+    public int maxHp = 5;
+
+    private int currentHp;
     private Rigidbody2D rb;
     private bool isDead = false;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
+        currentHp = maxHp;
     }
 
-    // a játékos sebzése ha belemegy az enemybe (BODY)
+    // Játékos nekiütközik
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (isDead) return;
 
         if (collision.gameObject.CompareTag("Player"))
         {
-            // megnézzük felülről jött-e
             bool hitFromAbove = collision.GetContact(0).normal.y > 0.1f;
-
 
             if (hitFromAbove)
             {
-                Die(collision.gameObject);
+                TakeDamage(collision.gameObject);
             }
             else
             {
@@ -35,14 +38,31 @@ public class EnemyFinalBoss : MonoBehaviour
         }
     }
 
-    // az  enemy elszublimálása ha eltapossák (HEAD TRIGGER)
+    // Fej trigger
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (isDead) return;
 
         if (other.CompareTag("Player"))
         {
-            Die(other.gameObject);
+            TakeDamage(other.gameObject);
+        }
+    }
+
+    void TakeDamage(GameObject player)
+    {
+        currentHp--;
+
+        // A játékos minden találat után pattanjon vissza
+        Rigidbody2D prb = player.GetComponent<Rigidbody2D>();
+        if (prb != null)
+        {
+            prb.linearVelocity = new Vector2(prb.linearVelocity.x, bounceForce);
+        }
+
+        if (currentHp <= 0)
+        {
+            Die();
         }
     }
 
@@ -54,26 +74,17 @@ public class EnemyFinalBoss : MonoBehaviour
         {
             pm.hp -= damage;
 
-            // KNOCKBACK IRÁNY
             Vector2 dir = (player.transform.position - transform.position).normalized;
-            dir.y = 0.8f; // kicsit felfelé is dobja
+            dir.y = 0.8f;
 
             pm.Knockback(dir, 6f);
         }
     }
 
-    void Die(GameObject player)
+    void Die()
     {
         isDead = true;
 
-        // játékos bounce
-        Rigidbody2D prb = player.GetComponent<Rigidbody2D>();
-        if (prb != null)
-        {
-            prb.linearVelocity = new Vector2(prb.linearVelocity.x, bounceForce);
-        }
-
-        // enemy eltűnik
         Destroy(gameObject);
     }
 }
